ea762b63cb3f46d55bbdeee8b5a9aa97f7ab4a1f
1939108122 <1939108122@qq.com>
2020-06-01 23:13:36 +0800

    头部组件制作

---
+++ blog/components/Header.js
@@ -0,0 +1,33 @@
+import '../public/components/header.css'
+import { Row, Col, Menu } from 'antd'
+import { HomeOutlined, YoutubeOutlined, DribbbleOutlined } from '@ant-design/icons'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Row type="flex" justify="center">
+        <Col xs={24} sm={24} md={10} lg={10} xl={10}>
+          <span className="header-logo">基尼钛美俱乐部,</span>
+          <span className="header-txt">专注篮球训练</span>
+        </Col>
+        <Col xs={0} sm={0} md={14} lg={8} xl={6}>
+          <Menu mode="horizontal">
+            <Menu.Item key="home" icon={<HomeOutlined />}>
+                首页
+            </Menu.Item>
+
+            <Menu.Item key="video" icon={<YoutubeOutlined />}>
+                视频
+            </Menu.Item>
+
+            <Menu.Item key="basketball" icon={<DribbbleOutlined />}>
+                篮球
+            </Menu.Item>
+          </Menu>
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default Header
---
+++ blog/pages/_app.js
@@ -0,0 +1,5 @@
+import App from 'next/app'
+import 'antd/dist/antd.css'
+import '../public/pages/common.css'
+
+export default App
